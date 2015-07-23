bfe18ded4d31eee4e4ff154fd1bb56ac770fbfd9
Charlon Palacay <charlon@gmail.com>
2015-07-23 11:40:19 -0700

    Adds static template views and urls.

---
+++ mdotdevs/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, include, url
+
+#from django.contrib import admin
+#admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    
+    url(r'^$', 'mdotdevs.views.home', name='home'),
+    
+    # url(r'^blog/', include('blog.urls')),
+    #url(r'^admin/', include(admin.site.urls)),
+    
+    # include applications
+    #url(r'^', include('app_name.urls')),
+)
--- mdotdevs/views.py
+++ mdotdevs/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
-# Create your views here.
+import urllib
+import json
+
+# create your views here
+
+def home(request):
+    return render_to_response('mdotdevs/home.html', context_instance=RequestContext(request))

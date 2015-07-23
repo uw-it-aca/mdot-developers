d11e3d0e4e0e3b90bc95152707927c65eefd1e84
Charlon Palacay <charlon@gmail.com>
2015-07-23 11:08:31 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+
+#!/usr/bin/env python
+
+# Taken from django's setup.py:
+
+import os
+
+from distutils.core import setup
+import setuptools
+
+setup(name='mdot_developers',
+      version='1.0.4',
+      license = "Apache 2.0",
+      author = "UW-IT ACA",
+      author_email = "pmichaud@uw.edu",
+      packages=setuptools.find_packages(exclude=["project"]),
+      include_package_data=True,  # use MANIFEST.in during install
+      url='https://github.com/uw-it-aca/mdot-developers',
+      description='Clients for a variety of RESTful web services at the University of Washington',
+      install_requires=['Django<1.8', 'lxml==2.3.5', 'urllib3==1.10.2', 'twilio==3.4.1', 'boto', 'simplejson>=2.1', 'djangorestframework>=2.0', 'jsonpickle>=0.4.0', 'ordereddict>=1.1', 'python-dateutil>=2.1', 'unittest2>=0.5.1', 'pytz', 'icalendar', 'AuthZ-Group>=1.1.4', 'Django-UserService'],
+     )

4b710bbb525102ae1b8d8872d750a876c51a29a4
Charlon Palacay <charlon@gmail.com>
2015-07-23 11:28:17 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='mdot_developers',
+    name='mdotdevs',
     version='0.1',
-    packages=['mdot_developers'],
+    packages=['mdotdevs'],
     include_package_data=True,
     install_requires = [
         'setuptools',

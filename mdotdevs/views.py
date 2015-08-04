521c9fcb40a3bb0cdc5ac55356e9deb1a35cdf51
Jason Civjan <jcivjan@uw.edu>
2015-08-04 11:23:47 -0700

    hashing out app review form using django forms module.

--- mdotdevs/views.py
+++ mdotdevs/views.py
@@ -1,9 +1,13 @@
 from django.conf import settings
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 import urllib
 import json
+
+from forms import ReviewForm
 
 # create your views here
 
@@ -16,6 +20,21 @@ def guidelines(request):
     
 def process(request):
     return render_to_response('mdotdevs/process.html', context_instance=RequestContext(request))
-    
+
 def review(request):
-    return render_to_response('mdotdevs/review.html', context_instance=RequestContext(request))
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = ReviewForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = ReviewForm()
+
+    return render(request, 'mdotdevs/review.html', {'form': form})
